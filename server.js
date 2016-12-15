ed6d55783f144c688cfcf65cf36709f14358465d
Steven Anderson <happygoatdesigns@gmail.com>
2016-12-15 15:47:24 -0700

    tested and working up to step 3. Need to Create viewer endpoint and test it with Postman though.

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var session = require('express-session');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// CONFIG //
+var config = require('./config');
+
+// APP //
+var app = module.exports = express();
+// app.use(express.static(__dirname + "./../public"));
+
+app.use(session({secret: config.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+  clientID: config.FACEBOOK_clientID,
+  clientSecret: config.FACEBOOK_clientSecret,
+  callbackURL: 'http://localhost:3021/auth/facebook/callback'
+}, function(token, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+// app.get('/auth/github', passport.authenticate('github'));
+// app.get('/auth/google', passport.authenticate('google'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var port = config.PORT;
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
